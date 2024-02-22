0252b33147b2a917c87f491c799becc9d996fceb
kevinscs <kevinsatiro123@gmail.com>
2024-02-21 21:23:55 -0300

    FIX: Corrigindo o contador e adicionando uma nova imagem de fundo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import useCountdown from './hooks/useCountdown';
 import './App.css';
 
 function App() {
-  const [day, hour, minute, second] = useCountdown("jan 1, 2024 00:00:00")
+  const [day, hour, minute, second] = useCountdown("jan 1, 2025 00:00:00")
 
   return (
     <div className='App' style={{backgroundImage: `url(${NewYear})`}}>
